0694e46b2ca18954608079796cdaf7c5e1bc98c5
JeffNguyen <jeffmn93@gmail.com>
2014-06-05 19:49:10 -0700

    updated sql precomputation

--- cse135Part3/sql.sql
+++ cse135Part3/sql.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     name        TEXT NOT NULL UNIQUE,
     role        TEXT NOT NULL,
     age   	INTEGER NOT NULL,
-    state  	TEXT NOT NULL
+    state  	INTEGER REFERENCES state (id) ON DELETE CASCADE
 );
 
 CREATE TABLE categories (
@@ -48,10 +48,10 @@ CREATE TABLE precomputeProdUser (
     sum      INTEGER NOT NULL
 )
 
-CREATE TABLE precomputeCategoryUser (
+CREATE TABLE precomputeProdState (
 	id	SERIAL PRIMARY KEY,
 	userID  INTEGER REFERENCES users (id) ON DELETE CASCADE,
-	cid	INTEGER REFERENCES categories (id) ON DELETE CASCADE,
+	prodID  INTEGER REFERENCES products (id) ON DELETE CASCADe,
 	sum     INTEGER NOT NULL
 )
 
@@ -60,15 +60,15 @@ SELECT u.id, p.id, sum(s.quantity*s.price)
 FROM products p, users u, sales s
 WHERE p.id = s.pid AND u.id = s.uid
 GROUP BY p.id, u.id
-ORDER BY u.id
+ORDER BY sum
 
 
-INSERT INTO precomputeCategoryUser(userID, cid, sum)
-SELECT u.id, c.id, sum(s.quantity*s.price)
-FROM categories c, users u, products p, sales s
-WHERE c.id = p.cid AND u.id = s.uid
-GROUP BY c.id, u.id
-ORDER BY u.id 
+INSERT INTO precomputeProdState(userID, prodID, sum)
+SELECT p.id, st.id, sum(s.quantity*s.price)
+FROM products p,sales s, state st, users u
+WHERE p.id = s.pid AND u.id = s.uid AND u.state = st.id
+GROUP BY p.id, st.id 
+ORDER BY sum
 
 INSERT INTO state(name)
 VALUES
